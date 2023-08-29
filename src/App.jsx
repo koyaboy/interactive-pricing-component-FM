6bc1ef70de87c13cb77094d3f22c895aa5d949f9
Koya <44787464+koyaboy@users.noreply.github.com>
2023-08-29 11:46:13 +0100

    Mobile-Design-Layout-Complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import iconCheck from "./assets/images/icon-check.svg"
 import iconSlider from "./assets/images/icon-slider.svg"
 
 function App() {
-  const [sliderValue, setSliderValue] = useState(0)
+  const [sliderValue, setSliderValue] = useState(50)
 
-  const thumbWidth = 58; // Adjust this value based on your thumb width
-  // const iconOffset = `${(-thumbWidth / 2 + (sliderValue - 1) * 100 / 99)}%`;
-  // const iconLeft = `${((sliderValue - 1) / (100 - 1)) * 100}`;
+  const calculateSliderGradient = () => {
+    const percentage = (sliderValue / 100) * 100;
+    return `linear-gradient(to right, hsl(174, 86%, 45%) ${percentage}%, hsl(224, 65%, 95%) 0% , hsl(224, 65%, 95%) 100%)`;
+  };
+
+  const sliderGradient = calculateSliderGradient();
 
   return (
     <>
@@ -23,52 +26,38 @@ function App() {
 
         <div className="flex justify-center">
           <div className="relative -top-44 bg-white flex flex-col items-center w-11/12 py-8 drop-shadow-2xl">
-            <div className="text-grayish-blue tracking-widest mb-8">100K PAGEVIEWS</div>
+            <div className="text-grayish-blue tracking-widest mb-12">100K PAGEVIEWS</div>
 
-            {/* <div className="relative  flex flex-col justify-center items-center w-full mb-8 px-3 bg-yellow"> */}
-
-            {/* <div className="relative w-full mb-8 flex justify-center">
+            <div className=" relative w-11/12 mb-12 flex justify-center ">
               <input
                 type="range"
-                className="range-slider"
-                min="1"
-                max="100"
-                value={sliderValue}
-                onChange={(e) => setSliderValue(e.target.value)}
-              />
-
-              <div className={`absolute w-fit -top-2 right-0.5  bg-red`}
-                style={{ left: `${iconLeft} `, }}
-              >
-                <img
-                  src={iconSlider}
-                  alt="icon-slider"
-                  className="w-10 h-6  "
-                />
-              </div>
-            </div> */}
-
-            <div className="relative w-full mb-8 px-3 bg-yellow">
-              <input
-                type="range"
-                className="range-slider"
                 min="0"
                 max="100"
+                step="25"
                 value={sliderValue}
                 onChange={(e) => setSliderValue(e.target.value)}
+                className="range-slider"
+                style={{ background: sliderGradient }}
               />
 
+              {/* <img
+                src={iconSlider}
+                alt="icon-slider"
+                className="absolute -top-0.5 -translate-x-1/2"
+                style={{
+                  left: `${sliderValue}%`,
+                  transform: `translate(${sliderValue === 0 ? '50%' :
+                    sliderValue === 25 ? '0%' :
+                      sliderValue === 50 ? '-50%' :
+                        sliderValue === 75 ? '-100%' :
+                          sliderValue === 100 ? '-150%' :
+                            '0' // Default value if none of the conditions are met
+                    })`,
+                }}
 
-              <div
-                className="absolute top-0 transform translate-x-1/4 bg-red"
-
-              >
-                <img src={iconSlider} alt="icon-slider" className="w-10 h-6" style={{ transform: `translateX(${((sliderValue - 1) / (100 - 1)) * 100}%)` }} />
-              </div>
+              /> */}
 
             </div>
-
-
 
 
             <div className="flex items-center mb-8">
@@ -76,12 +65,12 @@ function App() {
               <span className="text-sm text-grayish-blue">/ month</span>
             </div>
 
-            <div className=" relative flex justify-between items-center w-full mb-10">
-              <div>Monthly Billing</div>
+            <div className=" relative flex justify-between items-center w-full mb-10 pl-12 pr-4">
+              <div className="text-sm text-grayish-blue">Monthly Billing</div>
               <label className="switch">
                 <input type="checkbox" /><span className="slider round"></span>
               </label>
-              <div>Yearly Billing</div>
+              <div className="text-grayish-blue text-sm">Yearly Billing</div>
               <div className="bg-light-grayish-red text-light-red rounded-full px-1.5 py-0.5 text-sm">-25%</div>
             </div>
 
@@ -90,15 +79,15 @@ function App() {
             <div className="flex flex-col items-center mt-6">
               <div className="flex items-center gap-4 mb-2">
                 <img src={iconCheck} alt="icon-check" />
-                <div className="text-grayish-blue">Unlimited Websites</div>
+                <div className="text-grayish-blue text-sm">Unlimited Websites</div>
               </div>
               <div className="flex items-center gap-4 mb-2">
                 <img src={iconCheck} alt="icon-check" />
-                <div className="text-grayish-blue">100% data ownership</div>
+                <div className="text-grayish-blue text-sm">100% data ownership</div>
               </div>
               <div className="flex items-center gap-4 mb-8">
                 <img src={iconCheck} alt="icon-check" />
-                <div className="text-grayish-blue">Email reports</div>
+                <div className="text-grayish-blue text-sm">Email reports</div>
               </div>
               <button className="text-pale-blue bg-dark-desaturated-blue px-12 py-3 rounded-full">Start my trial</button>
             </div>
@@ -106,10 +95,7 @@ function App() {
 
           </div>
         </div >
-
-
-
-      </div >
+      </div>
     </>
   )
 }
